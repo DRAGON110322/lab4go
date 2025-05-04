58c90a7d4c778845e1447cb9ee4524ac3e56e6bc
DRAGON110322 <maximrinkevitch@gmail.com>
2025-05-04 23:06:42 +0300

    Update animal.go

--- animal/animal.go
+++ animal/animal.go
@@ -2,14 +2,12 @@ package animal
 
 import "fmt"
 
-// Animal представляет базовый тип животного.
 type Animal struct {
 	name    string
 	age     int
 	species string
 }
 
-// NewAnimal создает новый экземпляр Animal.
 func NewAnimal(name string, age int, species string) *Animal {
 	return &Animal{
 		name:    name,
@@ -18,32 +16,10 @@ func NewAnimal(name string, age int, species string) *Animal {
 	}
 }
 
-// Eat выводит сообщение о том, что животное ест.
 func (a *Animal) Eat() {
 	fmt.Printf("%s %s ест.\n", a.species, a.name)
 }
 
-// MakeFamily выводит сообщение о создании семьи.
-func (a *Animal) MakeFamily() {
-	fmt.Printf("%s %s создает семью.\n", a.species, a.name)
-}
-
-// Sleep выводит сообщение о том, что животное спит.
-func (a *Animal) Sleep() {
-	fmt.Printf("%s %s спит и видит сны.\n", a.species, a.name)
-}
-
-// Name возвращает имя животного.
 func (a *Animal) Name() string {
 	return a.name
-}
-
-// Age возвращает возраст животного.
-func (a *Animal) Age() int {
-	return a.age
-}
-
-// Species возвращает вид животного.
-func (a *Animal) Species() string {
-	return a.species
 }
