f48418f0e2661a8fdf013cb62fc4180e573983b0
DRAGON110322 <maximrinkevitch@gmail.com>
2025-05-04 22:29:20 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"example.com/animal"
-	"example.com/monkey"
-	"example.com/human"
-	"example.com/politician"
+	"github.com/animal"
+	"github.com/monkey"
+	"github.com/human"
+	"github.com/politician"
 	"math/rand"
 	"time"
 )
